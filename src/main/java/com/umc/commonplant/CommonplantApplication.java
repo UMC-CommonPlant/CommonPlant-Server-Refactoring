2a4f1d77ff8e2a53ad19e5414674055515cf00a5
hweyoung <gnldud0516@gmail.com>
2023-07-11 01:09:45 +0900

    [Chore] 프로젝트 초기 설정

---
+++ src/main/java/com/umc/commonplant/CommonplantApplication.java
@@ -0,0 +1,13 @@
+package com.umc.commonplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonplantApplication.class, args);
+	}
+
+}
