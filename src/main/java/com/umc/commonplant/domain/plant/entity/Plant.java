c23faa9dde8d182e7c7b04b952c30daf646fdd38
방지민 <bjimin7@naver.com>
2023-08-21 20:19:16 +0900

    [Chore] Multi datasource setting

--- src/main/java/com/umc/commonplant/domain/plant/entity/Plant.java
+++ src/main/java/com/umc/commonplant/domain/plant/entity/Plant.java
@@ -1,9 +1,8 @@
 package com.umc.commonplant.domain.plant.entity;
 
 import com.umc.commonplant.domain.BaseTime;
-import com.umc.commonplant.domain.info.entity.Info;
+import com.umc.commonplant.domain2.info.entity.Info;
 import com.umc.commonplant.domain.place.entity.Place;
-import com.umc.commonplant.domain.user.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
@@ -26,11 +25,6 @@ public class Plant extends BaseTime {
     @JoinColumn(name = "place_idx", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Place place;
-
-    @ManyToOne
-    @JoinColumn(name = "info_idx", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Info info;
 
     private String name;
     private String imgUrl;
