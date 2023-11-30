8255f729d5ac547013c5906f12284a0349b4f8a5
hweyoung <gnldud0516@gmail.com>
2023-11-30 21:09:57 +0900

    [Refact] delete story, qna

--- src/main/java/com/umc/commonplant/domain/story/entity/StoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.umc.commonplant.domain.story.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoryRepository extends JpaRepository<Story, Long> {
-}
--- src/main/java/com/umc/commonplant/global/exception/ErrorResponseStatus.java
+++ src/main/java/com/umc/commonplant/global/exception/ErrorResponseStatus.java
@@ -59,11 +59,6 @@ public enum ErrorResponseStatus {
 
     // 4700 : Alarm
 
-    // 4800 : Story
-    NO_SELECTED_STORY_IMAGE(false, 4300,"story의 이미지는 최소 1장, 최대 5장 입니다."),
-    NOT_FOUND_STORY(false, 4301, "등록된 스토리가 없습니다."),
-    NOT_FOUNT_PARENT_COMMENT(false, 4401, "등록된 댓글이 없습니다."),
-
     //5000 : Server connection 오류
     SERVER_ERROR(false, 5000, "서버와의 연결에 실패하였습니다."),
     DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
