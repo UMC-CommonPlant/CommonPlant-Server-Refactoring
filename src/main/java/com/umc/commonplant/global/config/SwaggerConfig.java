38c24039df010031827e5301031c318642c6a851
hweyoung <gnldud0516@gmail.com>
2024-01-14 15:53:52 +0900

    Chore : springfox -> springdoc 변경

--- src/main/java/com/umc/commonplant/global/config/SwaggerConfig.java
+++ src/main/java/com/umc/commonplant/global/config/SwaggerConfig.java
@@ -1,58 +1,36 @@
 package com.umc.commonplant.global.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
-    }
-
+public class SwaggerConfig {
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.umc.commonplant"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(metaData());
-    }
-    private ApiInfo metaData() {
-        return new ApiInfoBuilder()
-                .title("COMMONPLANT REST API TEST")
-                .description("Project Portpolio")
-                .version("0.1.0")
-                .termsOfServiceUrl("Terms of service")
-                .contact(new Contact("commonplant", "https://github.com/UMC-CommonPlant", ""))
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-                .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("apiKeyScheme",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-AUTH-TOKEN")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("apiKeyScheme"))
+                .info(apiInfo());
     }
 
-    private ApiInfo apiInfo() {
-        String description = "Welcome CommonPlant Api Test";
-        return new ApiInfoBuilder()
-                .title("SWAGGER TEST")
-                .description(description)
-                .version("1.0")
-                .build();
+    private Info apiInfo() {
+        return new Info()
+                .title("FamilyMoments API Document")
+                .description("FamilyMoments 2 API 명세서")
+                .version("0.4.2");
     }
-
 }
