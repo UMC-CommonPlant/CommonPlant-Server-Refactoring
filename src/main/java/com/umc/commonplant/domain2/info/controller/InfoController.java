f619fb0274817472871c4fe556d8f0dc7b208bc7
방지민 <bjimin7@naver.com>
2024-01-19 22:44:47 +0900

    [Feat] Information Swagger 적용

--- src/main/java/com/umc/commonplant/domain2/info/controller/InfoController.java
+++ src/main/java/com/umc/commonplant/domain2/info/controller/InfoController.java
@@ -3,8 +3,15 @@ package com.umc.commonplant.domain2.info.controller;
 import com.umc.commonplant.domain2.info.dto.InfoDto;
 import com.umc.commonplant.domain2.info.service.InfoService;
 import com.umc.commonplant.global.dto.JsonResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,11 +22,11 @@ import java.util.List;
 @RequestMapping("/info")
 @RequiredArgsConstructor
 @RestController
-public class InfoController {
+public class InfoController implements InfoSwagger{
     private final InfoService infoService;
 
-    @PostMapping("/addPlantInfo")
-    public ResponseEntity<JsonResponse> createInfo(@RequestPart("infoRequest") InfoDto.InfoRequest infoRequest, @RequestPart("image") MultipartFile multipartFile) {
+    @PostMapping(value = "/addPlantInfo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JsonResponse> createInfo(@RequestPart("infoRequest") InfoDto.InfoRequest infoRequest, @RequestPart(value = "image", required = false) MultipartFile multipartFile) {
 
         infoService.createInfo(infoRequest, multipartFile);
 
